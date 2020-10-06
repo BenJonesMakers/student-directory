74fcc6ff141017584400cd0bf810ca1b9dd5565b
Ben Jones <benwjones76@outlook.com>
2020-10-06 14:18:38 +0100

    removed the array of students and asked the user to enter them

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-# list of students as an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat the code
+  while !name.empty? do
+	  # add the student hash to the array
+    students << {name: name, cohort: :november}
+    # get another name from the user
+    name = gets.chomp
+  end
+	return students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -28,7 +31,7 @@ def print_footer(students)
 end
 
 # main program
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
