94fed430c89972c18db004a0f85cafb4d1978f5c
Ben Jones <benwjones76@outlook.com>
2020-10-06 12:18:52 +0100

    refactored into methods

--- directory.rb
+++ directory.rb
@@ -13,21 +13,23 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
 
-# print the names
-puts "The students of Villains Academy"
-puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+def print(names)
+  names.each do |name|
+  puts name
+  end
+end
 
-# Print the number of students
-puts "Overall, we have #{student_count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# main program
+
+print_header
+print(students)
+print_footer(students)
