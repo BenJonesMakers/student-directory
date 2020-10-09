a6effde4ecc5f5600b2ebb0d6473c79fda529515
Ben Jones <benwjones76@outlook.com>
2020-10-09 09:11:48 +0100

    loads students.csv by default

--- directory.rb
+++ directory.rb
@@ -74,7 +74,7 @@ end
 
 def try_load_students
   filename = ARGV.first # first get the arguement from the command line
-  return if filename.nil? # get out of the method if it isn't given
+  filename = "students.csv" if filename.nil?  # default to trying students.csv
   if File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
